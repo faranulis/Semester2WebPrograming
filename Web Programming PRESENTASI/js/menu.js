283cd4711627ee8a742aad8034d3820cde31ac84
Faran Ramdan <webvizer.com@gmail.com>
2021-06-14 19:40:31 +0700

    Add files via upload

---
+++ Web Programming PRESENTASI/js/menu.js
@@ -0,0 +1,16 @@
+document.getElementById("menu").innerHTML = "<div id='menukiri'>\
+		<a href=''>Home</a>\
+		<a href=''>Profile</a>\
+		<a href=''>Konten</a>\
+		<a href=''>Program Unggulan</a>\
+		<a href=''>Kegiatan</a>\
+		<a href=''>Visi Misi</a>\
+	</div>\
+	<div id='menukanan'>\
+	<a href=''><i class='fab fa-facebook'></i></a> \
+	<a href=''><i class='fab fa-instagram'></i></a>\
+	<a href=''><i class='fab fa-youtube'></i></a>\
+	</div>";
+
+document.getElementById("footer").innerHTML = "<h3>Presentasi Web Programming</h3>\
+  		<p>Tema : Sekolah, Content By <a href='contentby.html'>Kelompok 3</a></p>"
